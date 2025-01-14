4e3761f9e9e8ec664a0b3b2c5fb24d0aeca3dcc1
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2025-01-14 11:41:44 +0000

    Impliment ability to add multiple educations

--- src/App.jsx
+++ src/App.jsx
@@ -50,13 +50,31 @@ function Edit(props) {
         ))}
       </div>
       {props.title === "Education" && props.index === props.keyValue && (
-        <button>Submit</button>
+        <button
+          onClick={() =>
+            props.submitEducation({
+              School: props.formData["School"],
+              "Start Date": props.formData["Start Date"],
+              "End Date": props.formData["End Date"],
+              Degree: props.formData["Degree"],
+              Description: props.formData["Description"],
+            })
+          }
+        >
+          Submit
+        </button>
       )}
     </div>
   );
 }
 
-function Sidebar({ onInputChange, formData, handleIndexChange, index }) {
+function Sidebar({
+  onInputChange,
+  formData,
+  handleIndexChange,
+  index,
+  submitEducation,
+}) {
   return (
     <div className="sidebar">
       <Edit
@@ -79,24 +97,25 @@ function Sidebar({ onInputChange, formData, handleIndexChange, index }) {
         keyValue={1}
         key={1}
         formData={formData}
+        submitEducation={submitEducation}
       />
     </div>
   );
 }
 
-function EducationItem({ formData }) {
+function EducationItem({ schoolData }) {
   return (
     <div className="education-flex">
       <div>
-        <div className="school-name">{formData["School"]}</div>
+        <div className="school-name">{schoolData["School"]}</div>
         <div>
-          {formData["Start Date"]} {formData["Start Date"] && "--"}{" "}
-          {formData["End Date"]}
+          {schoolData["Start Date"]} {schoolData["Start Date"] && "--"}{" "}
+          {schoolData["End Date"]}
         </div>
       </div>
       <div>
-        <div>{formData["Degree"]}</div>
-        <div className="description">{formData["Description"]}</div>
+        <div className="school-name">{schoolData["Degree"]}</div>
+        <div className="description">{schoolData["Description"]}</div>
       </div>
     </div>
   );
@@ -114,10 +133,12 @@ function MainContent({ formData }) {
         </div>
       </div>
       <div className="CV-Education">
-        <h3>Education</h3>
+        <h3 className="education-title">Education</h3>
         {/* need to make this so that 1. it has good layout adn 2. so that you can add multiple educations */}
 
-        <EducationItem formData={formData} />
+        {formData["Schools"]?.map((school) => (
+          <EducationItem schoolData={school} key={school.School} />
+        ))}
       </div>
       <div>3</div>
     </div>
@@ -137,6 +158,21 @@ function App() {
     setEditIndex(editIndex === key ? null : key);
   };
 
+  const submitEducation = (schoolData) => {
+    const updatedData = {
+      ...formData,
+      Schools: formData.Schools
+        ? [...formData.Schools, schoolData]
+        : [schoolData],
+      School: "",
+      "Start Date": "",
+      "End Date": "",
+      Degree: "",
+      Description: "",
+    };
+    setFormData(updatedData);
+  };
+
   return (
     <div className="main-grid">
       <Sidebar
@@ -144,6 +180,7 @@ function App() {
         formData={formData}
         handleIndexChange={handleIndexChange}
         index={editIndex}
+        submitEducation={submitEducation}
       />
       <MainContent formData={formData} />
     </div>
